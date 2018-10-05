bf2377f7f2e2e13a9b2e3996addd94bcbf9bc9a8
Aditya Ch <ch.aditya1996@gmail.com>
2018-10-05 20:11:08 +0530

    Uploaded the project

---
+++ recommendation.m
@@ -0,0 +1,87 @@
+%  Movie Recommendation System using Collaborative Filtering
+
+% Load movie list
+movieList = loadMovieList();
+
+%  Initialize ratings
+my_ratings = zeros(1682, 1);
+
+% Enter ratings for a new user
+% Check the file movie_idx.txt for id of each movie in the dataset
+% Note: Ratings can be only from 1-5
+num = input("How many movie ratings would you like to input? ");
+for i = 1:num
+    id = input("Enter movie id: ");
+    rating = input("Enter rating: ");
+    my_ratings(id) = rating;
+end
+
+fprintf('\n\nUser ratings given are:\n');
+for i = 1:length(my_ratings)
+    if my_ratings(i) > 0 
+        fprintf('Rated %d for %s\n', my_ratings(i), ...
+                 movieList{i});
+    end
+end
+
+%  Learning Movie Ratings: Now, we will train the collaborative
+%  filtering model on a movie rating dataset of 1682 movies and 943 users
+
+fprintf('\nTraining collaborative filtering...\n');
+
+%  Load data
+load('movies.mat');
+
+%  Y is a 1682x943 matrix, containing ratings (1-5) of 1682 movies by 
+%  943 users
+%
+%  R is a 1682x943 matrix, where R(i,j) = 1 if and only if user j gave a
+%  rating to movie i
+
+%  Add our own ratings to the data matrix
+Y = [my_ratings Y];
+R = [(my_ratings ~= 0) R];
+
+%  Normalize Ratings
+[Ynorm, Ymean] = normalizeRatings(Y, R);
+
+num_users = size(Y, 2);
+num_movies = size(Y, 1);
+num_features = 10;
+
+% Set Initial Parameters (Theta, X)
+X = randn(num_movies, num_features);
+Theta = randn(num_users, num_features);
+
+initial_parameters = [X(:); Theta(:)];
+
+% Set options for fmincg
+options = optimset('GradObj', 'on', 'MaxIter', 100);
+
+% Set Regularization
+lambda = 10;
+theta = fmincg (@(t)(cofiCostFunc(t, Ynorm, R, num_users, num_movies, ...
+                                num_features, lambda)), ...
+                initial_parameters, options);
+
+% Unfold the returned theta back into U and W
+X = reshape(theta(1:num_movies*num_features), num_movies, num_features);
+Theta = reshape(theta(num_movies*num_features+1:end), ...
+                num_users, num_features);
+
+fprintf('Recommender system learning completed.\n');
+
+%  Recommendation for you
+
+p = X * Theta';
+my_predictions = p(:,1) + Ymean;
+
+movieList = loadMovieList();
+
+[r, ix] = sort(my_predictions, 'descend');
+fprintf('\nTop recommendations for you:\n');
+for i=1:10
+    j = ix(i);
+    fprintf('Predicting rating %.1f for movie %s\n', my_predictions(j), ...
+            movieList{j});
+end
